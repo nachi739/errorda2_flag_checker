22f7582ec5304097d255c1eff240b522f8354b99
namiki <namiki081025@gmail.com>
2024-09-25 18:09:18 +0900

    [update]フロント側の情報内容の追加・修正
    tagが連結して表示されていた箇所を修正
    詳細ページへの画面遷移のためにLinkを作成し動作確認
    ※まだ詳細ページは作成していない

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -1,6 +1,5 @@
-import { link } from "fs";
-import { title } from "process";
 import React from "react";
+import Link from "next/link";
 
 type Props = {
     title: string;
@@ -16,10 +15,12 @@ export const SinglePost = (props: Props) => {
     return (
         <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2x1 hover:shadow-none hover:translate-y-1 transition-all duration-300">
             <div className="flex items-center gap-3">
-                <h2 className="text-gray-100 text-2x1 font-medium mb-2">{title}</h2>
+                <h2 className="text-gray-100 text-2x1 font-medium mb-2">
+                    <Link href={`/posts/${slug}`}>{title}</Link>
+                </h2>
                 <div className="text-gray-100">{date}</div>
                 {tags.map((tag) => (
-                    <span className="text-white bg-gray-500 rounded-xl px-2 pb-0.5 font-medium">{tags}</span>
+                    <span className="text-white bg-gray-500 rounded-xl px-2 pb-0.5 font-medium">{tag}</span>
                 ))}
             </div>
             <p className="text-white">{description}</p>
