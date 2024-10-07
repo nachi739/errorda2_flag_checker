7e3ea06b8c76f74b2ff56b888304c9357de18303
namiki <namiki081025@gmail.com>
2024-10-07 21:59:09 +0900

    [fix]no-unused-varsへの対応

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 
-import { getNumberOfPages } from "@/lib/notionAPI";
 import { getPageLink } from "@/lib/page-helper";
 
 interface Props {
